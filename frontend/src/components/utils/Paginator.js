0e2bcae721d179e9d0e1603a32a1247502f775cc
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-07-27 21:45:32 -0300

    fix(frontend): Fix the problem when click in  next page

--- frontend/src/components/utils/Paginator.js
+++ frontend/src/components/utils/Paginator.js
@@ -1,83 +1,103 @@
-import React from 'react';
-import { 
-    Pagination, 
-    PaginationItem, 
-    PaginationLink 
+import React, { useState } from 'react';
+import {
+    Pagination,
+    PaginationItem,
+    PaginationLink
 } from 'reactstrap';
 
+import '../../styles/Paginator.css';
+
 function Paginator(props) {
+    const [active, setActive] = useState();
+
 
     function bindNextPage(page) {
         props.toggleNextPage(page);
     }
 
+    function bindActive(page) {
+        if (page === 'next') {
+            setActive(true);
+        }
+    }
+
     return (
-        <Pagination >
-            <PaginationItem>
-                <PaginationLink
-                    first
-                    href="#1"
-                    onClick={() => bindNextPage('1')}
-                />
-            </PaginationItem>
-
-
-            <PaginationItem>
-                {props.previousPage !== 0 ?
-                    <PaginationLink
-                        previous href={"#" + (props.previousPage)}
-                        onClick={() => bindNextPage(props.previousPage)}
-                    >
-                    </PaginationLink>
-                    : null}
-            </PaginationItem>
-
-            <PaginationItem>
-                {props.previousPage !== 0 ?
-                    <PaginationLink href={"#" + props.previousPage} onClick={() => bindNextPage(props.previousPage)}>
-                        {props.previousPage}
-                    </PaginationLink>
-
-                    : null}
-            </PaginationItem>
-
-            <PaginationItem>
-                <PaginationLink href={"#" + props.currentPage} onClick={() => bindNextPage(props.currentPage)}>
-                    {props.currentPage}
-                </PaginationLink>
-            </PaginationItem>
-
-            <PaginationItem>
-                {!(props.nextPage > props.pages) ?
-                    <PaginationLink href={"#" + props.nextPage} next onClick={() => bindNextPage(props.nextPage)}>
-                        {props.nextPage}
-                    </PaginationLink>
-                    : null}
-
-            </PaginationItem>
-
-            {!(props.nextPage > props.pages) ?
+        <div className="paginator-container">
+            <Pagination >
                 <PaginationItem>
                     <PaginationLink
-                        next
-                        href={"#" + (props.currentPage + 1)}
-                        onClick={() => bindNextPage(props.nextPage)}
+                        first
+                        href="#1"
+                        onClick={() => bindNextPage('1')}
                     />
                 </PaginationItem>
 
-                : null}
 
-            <PaginationItem>
-                <PaginationLink
-                    last
-                    href={"#" + props.pages}
-                    onClick={() => {
-                        bindNextPage(props.pages)
-                    }
-                    }
-                />
-            </PaginationItem>
-        </Pagination>
+                <PaginationItem >
+                    {props.previousPage !== 0 ?
+                        <PaginationLink
+                            previous href={"#" + (props.previousPage)}
+                            onClick={() => bindNextPage(props.previousPage)}
+                        >
+                        </PaginationLink>
+                        : null}
+                </PaginationItem>
+
+                <PaginationItem>
+                    {props.previousPage !== 0 ?
+                        <PaginationLink
+                            href={"#" + props.previousPage}
+                            onClick={() => bindNextPage(props.previousPage)}
+                        >
+                            {props.previousPage}
+                        </PaginationLink>
+                        : null}
+                </PaginationItem>
+
+                <PaginationItem active={() => active}>
+                    <PaginationLink
+                        href={"#" + props.currentPage}
+                        onClick={() => bindNextPage(props.currentPage)}
+                    >
+                        {props.currentPage}
+                    </PaginationLink>
+                </PaginationItem>
+
+                <PaginationItem >
+                    {!(props.nextPage > props.pages) ?
+                        <PaginationLink
+                            href={"#" + props.nextPage}
+                            onClick={() => {
+                                bindNextPage(props.nextPage);
+                                bindActive('next')
+                            }
+                            }
+                        >
+                            {props.nextPage}
+                        </PaginationLink>
+                        : null}
+
+                </PaginationItem>
+
+                {!(props.nextPage > props.pages) ?
+                    <PaginationItem >
+                        <PaginationLink
+                            next
+                            href={"#" + (props.currentPage + 1)}
+                            onClick={() => bindNextPage(props.nextPage)}
+                        />
+                    </PaginationItem>
+                    : null}
+
+                <PaginationItem >
+                    <PaginationLink
+                        last
+                        href={"#" + props.pages}
+                        onClick={() => { bindNextPage(props.pages) }}
+                    />
+                </PaginationItem>
+            </Pagination>
+        </div>
     )
 }
 
